36761ec79b42696cf2a056d75e6b265ff0c582f0
wei guojun <gwei@email.arizona.edu>
2020-12-07 02:28:03 -0700

    init

---
+++ CSC483finalproject/src/main/java/GuojunWei/CSC483finalproject/App.java
@@ -0,0 +1,13 @@
+package GuojunWei.CSC483finalproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
